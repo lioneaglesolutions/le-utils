152c5e8be0ab927c9feda4859d02d3bc73f7c7fc
James Mooring <james@lioneagle.solutions>
2021-08-24 17:30:57 +1000

    feat: Testing provider

--- src/LeUtilsServiceProvider.php
+++ src/LeUtilsServiceProvider.php
@@ -6,14 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class LeUtilsServiceProvider extends ServiceProvider
 {
-    // public function boot()
-    // {
-    //     if ($this->app->runningInConsole()) {
-    //         $this->publishes([
-    //             __DIR__ . '/../config/le-utils.php' => config_path('le-utils.php'),
-    //         ], 'le-utils-config');
-    //     }
-    // }
+    public function boot()
+    {
+        // if ($this->app->runningInConsole()) {
+        //     $this->publishes([
+        //         __DIR__ . '/../config/le-utils.php' => config_path('le-utils.php'),
+        //     ], 'le-utils-config');
+        // }
+        info('we made it');
+    }
 
     // public function register()
     // {
