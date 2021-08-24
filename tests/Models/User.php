3a3c72e65c7888397f3c8371416e662c29fffbdd
James Mooring <james@lioneagle.solutions>
2021-08-24 19:03:39 +1000

    refactor: Rename base Eloquent model

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -2,12 +2,12 @@
 
 namespace Lioneagle\LeUtils\Tests\Models;
 
-use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Lioneagle\LeUtils\Contracts\Uuidable;
 use Lioneagle\LeUtils\Traits\HasUuid;
 
-class User extends EloquentModel implements Uuidable
+class User extends Model implements Uuidable
 {
     use HasUuid;
 
