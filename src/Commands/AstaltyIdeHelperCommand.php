3a35c86b7bfe94359b9429b449bd9012b152e428
James Mooring <james@lioneagle.solutions>
2021-10-19 15:33:54 +1000

    feat: add ide helper command

---
+++ src/Commands/AstaltyIdeHelperCommand.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\Finder\Finder;
+
+class AstaltyIdeHelperCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'astalty:ide {--M|models} {--I|interactive} {--F|facades}';
+    
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate IDE helper files';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if ($this->option('models')) {
+            $this->generateModelDocs();
+        }
+
+        if ($this->option('facades')) {
+            $this->generateFacadeDocs();
+        }
+
+        return 0;
+    }
+
+    protected function generateFacadeDocs()
+    {
+        $this->call('ide-helper:generate');
+    }
+
+    protected function generateModelDocs()
+    {
+        if ($this->option('interactive')) {
+            return $this->generateModelDocsInteractively();
+        }
+
+        $this->call('ide-helper:models', [
+            '--write' => true
+        ]);
+    }
+
+    protected function generateModelDocsInteractively()
+    {
+        $models = $this->getModels();
+        $modelNames = $models->pluck('model_name')->all();
+        $choice = $this->choice('Select Models to generate docs for', $modelNames, null, null, true);
+
+        $selectedModels = $models->whereIn('model_name', $choice)->pluck('model')->all();
+
+        $this->call('ide-helper:models', [
+            'model' => $selectedModels,
+            '--write' => true
+        ]);
+    }
+
+    protected function getModels()
+    {
+        $modelPath = base_path('app/Models');
+
+        $namespace = App::getNamespace();
+
+        $files = (new Finder())->in($modelPath)->files()->depth(0);
+
+        return collect($files)
+            ->map(function ($file, $fileName) use ($namespace) {
+                $class =
+                    $namespace .
+                    str_replace(
+                        ['/', '.php'],
+                        ['\\', ''],
+                        Str::after(
+                            $file->getRealPath(),
+                            realpath(app_path()) . DIRECTORY_SEPARATOR
+                        )
+                    );
+
+                return [
+                    'model' => $class,
+                    'file' => $fileName,
+                    'model_name' => str_replace('App\\Models\\', '', $class),
+                ];
+            })
+            ->values();
+    }
+}
