549a2172c7f40088d0fee48786f8642466ece585
James Mooring <james@lioneagle.solutions>
2021-12-31 13:00:14 +1000

    update spies on actions trait to allow constructor args and return values

--- src/TestHelpers/Traits/SpiesOnActions.php
+++ src/TestHelpers/Traits/SpiesOnActions.php
@@ -3,14 +3,34 @@
 namespace Lioneagle\LeUtils\TestHelpers\Traits;
 
 use Lioneagle\LeUtils\TestHelpers\ActionSpy;
+use Mockery;
+use ReflectionClass;
+use ReflectionParameter;
 
 /**
  * @mixin \Illuminate\Foundation\Testing\TestCase
  */
 trait SpiesOnActions
 {
+    /**
+     * @param  string  $action
+     * @return \Lioneagle\LeUtils\TestHelpers\ActionSpy
+     * @throws \ReflectionException
+     */
     public function spyOnAction(string $action): ActionSpy
     {
-        return new ActionSpy($this->spy($action)->makePartial());
+        $reflection = new ReflectionClass($action);
+
+        $reflectionArgs = $reflection->getConstructor()?->getParameters() ?: [];
+
+        $args = collect($reflectionArgs)->map(function (ReflectionParameter $parameter) {
+            return app($parameter->getType()->getName());
+        })->all();
+
+        $mock = Mockery::mock($action, $args)->shouldIgnoreMissing()->makePartial();
+
+        $spy = $this->instance($action, $mock);
+
+        return new ActionSpy($mock);
     }
 }
